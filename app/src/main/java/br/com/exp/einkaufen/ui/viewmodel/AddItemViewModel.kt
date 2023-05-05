c438ad9b41a716cfea673917c0c099ed11ac8764
EXPedro <pedroelisio555@gmail.com>
2023-05-05 11:37:57 -0300

    update: viewmodel

--- app/src/main/java/br/com/exp/einkaufen/ui/viewmodel/AddItemViewModel.kt
+++ app/src/main/java/br/com/exp/einkaufen/ui/viewmodel/AddItemViewModel.kt
@@ -1,16 +1,27 @@
 package br.com.exp.einkaufen.ui.viewmodel
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.room.RoomDatabase
+import br.com.exp.einkaufen.data.AppDatabase
+import br.com.exp.einkaufen.data.Item
+import br.com.exp.einkaufen.data.ItemRepository
 
-class AddItemViewModel: ViewModel() {
+class AddItemViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: ItemRepository = TODO()
+    private var readAll: LiveData<List<Item>>
     private var _newItems = MutableLiveData<String>()
     val newItems: LiveData<String>
         get() = _newItems
 
     init {
+        val db = AppDatabase.getDatabase(application).itemDao()
+        repository = ItemRepository(db)
+//        readAll = (repository.getAll())
         Log.i(ADD_ITEM_VIEW_MODEL, "ViewModel criado!" )
     }
 
